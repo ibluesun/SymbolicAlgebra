c849dc37dbe0fc0bd77717e6f5cfeeb837060fc3
IBlueSun_cp <SND\IBlueSun_cp>
2010-05-11 19:45:26 +0000

    // Issue 1 when multiplying x * u^2-2*v*u+v^2  the middle term was -2*x*v because only 'v' from middle term attached to fused variables //   fixed with adding all fused variables from middle term.
    // Issue 2 when dividing x / u^2-2*v*u+v^2  the number changed.

--- SymbolicAlgebra/SymbolicVariable_Calc.cs
+++ SymbolicAlgebra/SymbolicVariable_Calc.cs
@@ -10,61 +10,38 @@ namespace SymbolicAlgebra
     public partial class SymbolicVariable : ICloneable
     {
 
-
         #region coeffecient multiplication
 
         #region number precede symbol
         public static SymbolicVariable operator *(int a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
 
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) * b;
 
             return sv;
         }
 
         public static SymbolicVariable operator *(float a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) * b;
 
             return sv;
         }
 
         public static SymbolicVariable operator *(double a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) * b;
             return sv;
         }
 
         public static SymbolicVariable operator *(short a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) * b;
             return sv;
         }
         public static SymbolicVariable operator *(long a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) * b;
             return sv;
         }
         #endregion
@@ -73,125 +50,70 @@ namespace SymbolicAlgebra
         #region number after symbol
         public static SymbolicVariable operator *(SymbolicVariable b, int a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-            
-
+            var sv = b * new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
             return sv;
         }
 
         public static SymbolicVariable operator *(SymbolicVariable b, float a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-
+            var sv = b * new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
             return sv;
         }
 
         public static SymbolicVariable operator *(SymbolicVariable b, double a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-
+            var sv = b * new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
             return sv;
         }
 
         public static SymbolicVariable operator *(SymbolicVariable b, short a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-
+            var sv = b * new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
             return sv;
         }
         public static SymbolicVariable operator *(SymbolicVariable b, long a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient *= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient *= a;
-
+            var sv = b * new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
             return sv;
         }
         #endregion
 
         #endregion
-
-
 
         #region coeffecient division
 
         #region number precede symbol
         public static SymbolicVariable operator /(int a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
 
-            sv.Coeffecient = a / sv.Coeffecient;
-
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient = a / v.Value.Coeffecient;
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) / b;
 
             return sv;
         }
 
         public static SymbolicVariable operator /(float a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient = a / sv.Coeffecient;
-
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient = a / v.Value.Coeffecient;
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) / b;
 
             return sv;
         }
 
         public static SymbolicVariable operator /(double a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient = a / sv.Coeffecient;
-
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient = a / v.Value.Coeffecient;
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) / b;
 
             return sv;
         }
 
         public static SymbolicVariable operator /(short a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient = a / sv.Coeffecient;
-
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient = a / v.Value.Coeffecient;
-
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) / b;
 
             return sv;
         }
         public static SymbolicVariable operator /(long a, SymbolicVariable b)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient = a / sv.Coeffecient;
-
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient = a / v.Value.Coeffecient;
-
+            var sv = new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture)) / b;
 
             return sv;
         }
@@ -201,57 +123,39 @@ namespace SymbolicAlgebra
         #region number after symbol
         public static SymbolicVariable operator /(SymbolicVariable b, int a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient /= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient /= a;
+            var sv = b / new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
 
             return sv;
         }
 
         public static SymbolicVariable operator /(SymbolicVariable b, float a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient /= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient /= a;
+            var sv = b / new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
 
             return sv;
         }
 
         public static SymbolicVariable operator /(SymbolicVariable b, double a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient /= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient /= a;
+            var sv = b / new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
 
             return sv;
         }
 
         public static SymbolicVariable operator /(SymbolicVariable b, short a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient /= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient /= a;
+            var sv = b / new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
 
             return sv;
         }
+
         public static SymbolicVariable operator /(SymbolicVariable b, long a)
         {
-            var sv = (SymbolicVariable)b.Clone();
-
-            sv.Coeffecient /= a;
-            if (sv.AddedTerms.Count > 0)
-                foreach (var v in sv.AddedTerms) v.Value.Coeffecient /= a;
+            var sv = b / new SymbolicVariable(a.ToString(CultureInfo.InvariantCulture));
 
             return sv;
         }
+
         #endregion
 
         #endregion
