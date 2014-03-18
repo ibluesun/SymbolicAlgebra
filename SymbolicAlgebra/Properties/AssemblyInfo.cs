f10ecb63584807f6f7703b6627aebb3fa8452cb7
IBlueSun_cp <SND\IBlueSun_cp>
2014-03-18 10:47:51 +0000

    Symbolic Algebra [0.8.91] ----------------------- - Changed the Pow  function to call RaiseToSymbolicPower

--- SymbolicAlgebra/Properties/AssemblyInfo.cs
+++ SymbolicAlgebra/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.9")]
-[assembly: AssemblyFileVersion("0.8.9")]
+[assembly: AssemblyVersion("0.8.91")]
+[assembly: AssemblyFileVersion("0.8.91")]
